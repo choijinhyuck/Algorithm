2e2d8f2376ce70700f243a39ff4a5f9251797ef8
choijinhyuck <85658353+choijinhyuck@users.noreply.github.com>
2023-03-17 23:28:18 +0900

    Create 12985.py

---
+++ Solution/2017 Tips town/12985.py
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12985
+# 예상 대진표, Lv 1
+
+def solution(n,a,b):
+    count = 0
+    while a != b:
+        a = (a + 1)//2 if a % 2 else a//2
+        b = (b + 1)//2 if b % 2 else b//2
+        count += 1
+    return count

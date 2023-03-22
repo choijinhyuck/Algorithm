3afb9099ad54932e19a93469525a6f40c46cb4c9
choijinhyuck <85658353+choijinhyuck@users.noreply.github.com>
2023-03-23 08:27:37 +0900

    Update 62284.sql

--- Solution/Summer, winter coding 2019/62284.sql
+++ Solution/Summer, winter coding 2019/62284.sql
@@ -1,4 +1,9 @@
 /* https://school.programmers.co.kr/learn/courses/30/lessons/62284
 우유와 요거트가 담긴 장바구니 */
 
-SELECT A
+SELECT CART_ID
+FROM CART_PRODUCTS
+WHERE NAME IN ('YOGURT', 'MILK')
+GROUP BY CART_ID
+HAVING COUNT(DISTINCT NAME) = 2
+ORDER BY CART_ID;

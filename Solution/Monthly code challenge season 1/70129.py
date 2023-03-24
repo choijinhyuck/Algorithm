e5421e6b6c6640774680b8a31a1a28c4eb44c45f
choijinhyuck <85658353+choijinhyuck@users.noreply.github.com>
2023-03-24 12:56:17 +0900

    Create 70129.py

---
+++ Solution/Monthly code challenge season 1/70129.py
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/70129
+# 이진 변환 반복하기, Lv 2
+
+def convert(s, count):
+    count += s.count('0')
+    return format(s.count('1'), 'b'), count
+
+def solution(s):
+    count = 0
+    con_count = 0
+    while s != '1':
+        s, count = convert(s, count)
+        con_count += 1
+    return [con_count, count]

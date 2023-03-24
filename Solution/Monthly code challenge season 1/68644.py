44d1517b2feed4272fcac65a15aefe65835e03b6
choijinhyuck <85658353+choijinhyuck@users.noreply.github.com>
2023-03-24 13:09:17 +0900

    Create 68644.py

---
+++ Solution/Monthly code challenge season 1/68644.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/68644
+# 두 개 뽑아서 더하기, Lv 1
+
+from itertools import combinations
+
+def solution(numbers):
+    return sorted(list(set(map(sum, combinations(numbers, 2)))))
